668d4a7b92cf74eadc421a2f3302e2128a236d92
MTaurone <arshiahf@gmail.com>
2018-01-05 15:43:40 -0800

    Removed dead code Changed to change a pointer instead of return a value

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -2,17 +2,12 @@ use std::io::{self};
 use std::string::String;
 use std::process::{exit};
 
-pub fn request_input(mut in_str:String) -> String
+pub fn request_input(mut in_str:String)
 {
-    //let mut full_input = Vec::new();
-
     let i = io::stdin();
-    //let mut in_str = String::new();
     match i.read_line(&mut in_str)
     {
         Ok(_n) => {
-            println!("{}", in_str);
-            return in_str
         }
         Err(err) => {
             eprintln!("Error: {:?}", err);
