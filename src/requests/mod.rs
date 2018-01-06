226a3f25e2f1c9175915f86d2b4c6d4cbbf0dd8a
MTaurone <arshiahf@gmail.com>
2018-01-05 17:09:56 -0800

    Removed some useless whitespace Changed to allow raw str value as input

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -6,25 +6,18 @@ use std::process::{exit};
 fn flush_out(message: String)
 {
     let mut o = BufWriter::new(stdout());
-    match o.write(&message.into_bytes())
-    {
-        _ => {}
-    }
-
-    match o.flush()
-    {
-        _ => {}
-    }
+    match o.write(&message.into_bytes()){_ => {}}
+    match o.flush(){_ => {}}
 }
 
-pub fn request_input(message: String, in_str: &mut String)
+pub fn request_input(message: &str, in_str: &mut String)
 {
-    flush_out(message);
+    let flushable = String::from(message);
+    flush_out(flushable);
     let i = stdin();
     match i.read_line(in_str)
     {
-        Ok(_) => {
-        }
+        Ok(_) => {}
         Err(err) => {
             eprintln!("Error: {:?}", err);
             exit(1);
